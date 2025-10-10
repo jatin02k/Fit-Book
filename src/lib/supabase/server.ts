ab2a4e3d509148357bafb75ea7292474383e3ef7
Jatin <jatin02kr@gmail.com>
2025-10-11 03:43:58 +0530

    first commit

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,30 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import type { Database } from "../../types/supabase";
+
+
+export const createClient = async (): Promise<ReturnType<typeof createServerClient<Database>>> => {
+    const cookieStore = await cookies();
+    return createServerClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll(): { name: string; value: string }[] {
+                    return cookieStore.getAll();
+                },
+                setAll(cookiesToSet: { name: string; value: string; options?: CookieOptions }[]): void {
+                    try {
+                        cookiesToSet.forEach(({ name, value, options }) => {
+                            cookieStore.set(name, value, options);
+                        });
+                    } catch {
+                        // The `setAll` method was called from a Server Component.
+                        // This can be ignored if you have middleware refreshing
+                        // user sessions.
+                    }
+                },
+            },
+        }
+    );
+};  
