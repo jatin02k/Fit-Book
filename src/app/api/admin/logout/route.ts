0677f86a0c0afbb998ba3218ad1af4c00f142c73
Jatin <jatin02kr@gmail.com>
2025-11-01 21:35:19 +0530

    logout

---
+++ src/app/api/admin/logout/route.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export default async function POST(request: Request) {
+  try {
+    const supabase = await createClient();
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Supabase Logout error", error);
+      return NextResponse.json({ erro: "Failed to sign Out" }, { status: 500 });
+    }
+    return NextResponse.json(
+      { message: "Logged out successfully." },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Logout handler error:", error);
+    return NextResponse.json(
+      { error: "Internal Server error during logout." },
+      { status: 500 }
+    );
+  }
+}
